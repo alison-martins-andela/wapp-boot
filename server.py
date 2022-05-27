acb554cc3e6d7c41065754bfd03bfc240ba363c4
Alison Martins <alison.martins@pluto.tv>
2022-05-27 11:14:36 -0300

    Update server.py

--- server.py
+++ server.py
@@ -9,32 +9,29 @@ VERIFY_TOKEN = "a1993ff202a0f1b4ffae"
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        
         query = parse.urlparse(self.path).query
+        path = parse.urlparse(self.path).path
         query_components = dict(qc.split("=") for qc in query.split("&"))
-        print(str(query_components))
-        
-        mode = query_components['hub.mode']
-        token = query_components['hub.verify_token']
-        challenge = query_components['hub.challenge']
-    
 
-        if not(mode is None) and not(token is None):
-  
-  
-            if (mode == 'subscribe') and (token == VERIFY_TOKEN):
-      
-                print('WEBHOOK_VERIFIED')
-                self.send_response(HTTPStatus.OK)
-                self.end_headers()
-                msg = challenge
-                self.wfile.write(msg.encode())
+        if path == "/verification":
+            mode = query_components['hub.mode']
+            token = query_components['hub.verify_token']
+            challenge = query_components['hub.challenge']
 
-            else:
-                self.send_response(HTTPStatus.FORBIDDEN)
-                self.end_headers()   
+            if mode is not None and token is not None:
+                if (mode == 'subscribe') and (token == VERIFY_TOKEN): 
+                    print('WEBHOOK_VERIFIED')
+                    self.send_response(HTTPStatus.OK)
+                    self.end_headers()
+                    msg = challenge
+                    self.wfile.write(msg.encode())
+                else:
+                    self.send_response(HTTPStatus.FORBIDDEN)
+                    self.end_headers()   
         
 
-port = int(os.getenv('PORT', 82))
+port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
 httpd = socketserver.TCPServer(('', port), Handler)
 httpd.serve_forever()
